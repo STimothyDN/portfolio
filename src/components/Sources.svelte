77f49777c343a26bb9b0809dc691fdcbb66df26a
Timothy Navarro <stimothydn@gmail.com>
2023-05-01 08:19:45 -0400

    modal + data

---
+++ src/components/Sources.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Modal from './Modal.svelte';
+
+	let showModal = false;
+</script>
+
+<button on:click={() => (showModal = true)}>
+	show sources
+</button>
+
+<Modal bind:showModal>
+	<h2 slot="header">
+		Sources
+	</h2>
+
+	<ol class="definition-list">
+		<li>​​https://justwomenssports.com/reads/north-carolina-unc-soccer-college-cup-dominance/#:~:text=Of%20the%2040%20national%20championships,total%20Atlantic%20Coast%20Conference%20championships.</li>
+		<li>
+			https://en.wikipedia.org/wiki/North_Carolina_Tar_Heels_women%27s_soccer
+		</li>
+		<li>https://goheels.com/news/2022/9/14/womens-soccer-thirty-years-ago-carolina-womens-soccer-conquered-the-impossible.aspx</li>
+		<li>https://fanbuzz.com/soccer/unc-womens-soccer-dynasty/</li>
+		<li>https://www.ncaa.com/news/soccer-women/article/2020-01-03/womens-soccer-teams-most-ncaa-di-national-championships</li>
+		<li>https://goheels.com/sports/2012/8/1/205671583</li>
+	</ol>
+</Modal>
--- src/components/data.js
+++ src/components/data.js
@@ -7,13 +7,14 @@ export default {
 	children: [
 		{
 			name: 'Wins',
+			about: 'The number of wins the UNC Womens Soccer Team has had each year.',
 			children: [
 				{
 					name: '1979 - 1983',
 					about: 'The beginnings of a dynasty.',
 					children: [
 						{ name: '1979', value: 10, about: 'The UNC Womens Soccer Team was established and quickly gained recognition in collegiate soccer.'},
-						{ name: '1980', value: 21, about: 'AIAW Semifinals'},
+						{ name: '1980', value: 21, about: 'The team reached the AIAW National Championships semifinals, showing great potential.'},
 						{ name: '1981', value: 23, about: 'UNC captured their first AIAW National Championship, foreshadowing their future dominance.' },
 						{ name: '1982', value: 19, about: 'The Tar Heels claimed their first NCAA National Championship, beginning a new era in womens soccer.' },
 						{ name: '1983', value: 19, about: 'The team won back-to-back NCAA titles, solidifying their position as a collegiate powerhouse.' }
@@ -24,91 +25,93 @@ export default {
 					about: 'Building up steam.',
 					children: [
 						{ name: '1984', value: 24, about: 'The team won back-to-back NCAA titles, solidifying their position as a collegiate powerhouse.' },
-						{ name: '1985', value: 18, about: 'Runner up' },
+						{ name: '1985', value: 18, about: 'The Tar Heels reached the NCAA finals but finished as runners-up, narrowly missing a fourth title.' },
 						{ name: '1986', value: 24, about: 'The Tar Heels begin an unparalleled nine-year championship streak, showcasing their dominance.' },
-						{ name: '1987', value: 23 },
-						{ name: '1988', value: 18 }
+						{ name: '1987', value: 23, about: 'The team won their fifth NCAA title, continuing their historic championship run.' },
+						{ name: '1988', value: 18, about: 'UNC claimed their sixth consecutive NCAA title, showcasing their soccer dominance.' }
 					]
 				},
 				{
 					name: '1989 - 1993',
 					about: 'Dominance solidified',
 					children: [
-						{ name: '1989', value: 24 },
-						{ name: '1990', value: 20 },
-						{ name: '1991', value: 24 },
-						{ name: '1992', value: 25 },
-						{ name: '1993', value: 1 }
+						{ name: '1989', value: 24, about: 'The Tar Heels secured their seventh straight NCAA title, maintaining their winning streak.' },
+						{ name: '1990', value: 20, about: 'UNC won their eighth consecutive NCAA title, further solidifying their legacy.' },
+						{ name: '1991', value: 24, about: 'The team claimed their ninth straight NCAA title, extending their unrivaled success.' },
+						{ name: '1992', value: 25, about: 'UNC captured their 10th consecutive NCAA title, marking a decade of dominance.' },
+						{ name: '1993', value: 23, about: 'The Tar Heels won their 11th straight NCAA title, continuing their remarkable streak.' }
 					]
 				},
 				{
 					name: '1994 - 1998',
 					about: 'Reputation building',
 					children: [
-						{ name: '1994', value: 1 },
-						{ name: '1995', value: 0, about: 'Semifinals' },
-						{ name: '1996', value: 1, about: 'The team reclaimed their championship status, winning back-to-back NCAA titles.' },
-						{ name: '1997', value: 1, about: 'The team reclaimed their championship status, winning back-to-back NCAA titles.' },
-						{ name: '1998', value: 0, about: 'Runner up' }
+						{ name: '1994', value: 25, about: 'UNC claimed their 12th consecutive and 13th overall NCAA title, concluding their historic run.' },
+						{ name: '1995', value: 25, about: 'The team finished as NCAA runners-up, ending their championship streak.' },
+						{ name: '1996', value: 25, about: 'UNC bounced back, winning their 14th NCAA title and starting a new era of success.' },
+						{ name: '1997', value: 27, about: 'The Tar Heels secured their 15th NCAA title, winning back-to-back championships.' },
+						{ name: '1998', value: 25, about: 'UNC reached the NCAA quarterfinals, demonstrating their sustained excellence.' }
 					]
 				},
 				{
 					name: '1999 - 2003',
 					about: 'Competition stiffens',
 					children: [
-						{ name: '1999', value: 1, about: 'The Tar Heels secured consecutive national championships, furthering their exceptional legacy.' },
-						{ name: '2000', value: 1, about: 'The Tar Heels secured consecutive national championships, furthering their exceptional legacy.' },
-						{ name: '2001', value: 0, about: 'Runner up' },
-						{ name: '2002', value: 0, about: 'Semifinals' },
-						{ name: '2003', value: 1, about: 'UNC claimed their 18th NCAA title, reasserting their dominance in collegiate soccer.' }
+						{ name: '1999', value: 24, about: 'The team claimed their 16th NCAA title, continuing their remarkable championship legacy.' },
+						{ name: '2000', value: 21, about: 'UNC won their 17th NCAA title, capturing back-to-back national championships.' },
+						{ name: '2001', value: 24, about: 'The Tar Heels advanced to the NCAA semifinals, displaying their continued competitiveness.' },
+						{ name: '2002', value: 21, about: 'The team reached the NCAA quarterfinals, maintaining their reputation as a national powerhouse.' },
+						{ name: '2003', value: 27, about: 'UNC claimed their 18th NCAA title, reasserting their dominance in collegiate soccer.' }
 					]
 				},
 				{
 					name: '2004 - 2008',
 					about: 'Tar Heels remain competitive',
 					children: [
-						{ name: '2004', value: 0 },
-						{ name: '2005', value: 0 },
-						{ name: '2006', value: 1, about: 'The team won their 19th NCAA title, reestablishing their championship pedigree.' },
-						{ name: '2007', value: 0 },
-						{ name: '2008', value: 1, about: 'The Tar Heels captured their 20th NCAA title, asserting their prowess on the national stage.' }
+						{ name: '2004', value: 20, about: 'The team reached the NCAA third round, showcasing their resilience and skill.' },
+						{ name: '2005', value: 23, about: 'UNC reached the NCAA quarterfinals, demonstrating their sustained excellence.' },
+						{ name: '2006', value: 27, about: 'The team won their 19th NCAA title, reestablishing their championship pedigree.' },
+						{ name: '2007', value: 19, about: 'UNC reached the NCAA quarterfinals, maintaining their reputation as a national powerhouse.' },
+						{ name: '2008', value: 25, about: 'The Tar Heels captured their 20th NCAA title, asserting their prowess on the national stage.' }
 					]
 				},
 				{
 					name: '2009 - 2013',
 					about: 'The dynasty begins to crumble',
 					children: [
-						{ name: '2009', value: 1, about: 'UNC won their 21st national championship, further solidifying their place in womens soccer history.' },
-						{ name: '2010', value: 0 },
-						{ name: '2011', value: 0 },
-						{ name: '2012', value: 1, about: 'The Tar Heels claimed their 22nd NCAA title, reemphasizing their championship caliber. This is their last NCAA championship title.' },
-						{ name: '2013', value: 0 }
+						{ name: '2009', value: 23, about: 'UNC won their 21st national championship, further solidifying their place in womens soccer history.' },
+						{ name: '2010', value: 19, about: 'The team reached the NCAA second round, demonstrating their continued competitiveness.' },
+						{ name: '2011', value: 13, about: 'UNC made it to the NCAA third round, maintaining their strong presence in collegiate soccer.' },
+						{ name: '2012', value: 15, about: 'The Tar Heels claimed their 22nd NCAA title, reemphasizing their championship caliber. This is their last NCAA championship title.' },
+						{ name: '2013', value: 20, about: 'UNC reached the NCAA quarterfinals, highlighting their sustained excellence.' }
 					]
 				},
 				{
 					name: '2014 - 2018',
+					about: 'Rebuilding a dynasty',
 					children: [
-						{ name: '2014', value: 0 },
-						{ name: '2015', value: 0 },
-						{ name: '2016', value: 0, about: 'Semifinals' },
-						{ name: '2017', value: 0 },
-						{ name: '2018', value: 0, about: 'Runner up' }
+						{ name: '2014', value: 14, about: 'The team advanced to the NCAA quarterfinals, continuing their strong performance.' },
+						{ name: '2015', value: 15, about: 'UNC reached the NCAA quarterfinals once again, maintaining their competitive edge.' },
+						{ name: '2016', value: 17, about: 'The Tar Heels advanced to the NCAA semifinals, proving to be a force to be reckoned with.' },
+						{ name: '2017', value: 17, about: 'The team reached the NCAA quarterfinals, showcasing their unwavering competitiveness.' },
+						{ name: '2018', value: 21, about: 'UNC finished as NCAA runners-up, narrowly missing their 23rd championship title.' }
 					]
 				},
 				{
-					name: '2019 - 2023',
+					name: '2019 - 2022',
+					about: 'Tar Heels now.',
 					children: [
-						{ name: '2019', value: 0, about: 'Runner up' },
-						{ name: '2020', value: 0, about: 'Semifinals' },
-						{ name: '2021', value: 0 },
-						{ name: '2022', value: 0, about: 'Runner up' },
-						{ name: '2023', value: 0, about: 'Runner up' }
+						{ name: '2019', value: 24, about: 'The Tar Heels advanced to the NCAA finals, finishing as runners-up once more.' },
+						{ name: '2020', value: 18, about: 'Semifinals' },
+						{ name: '2021', value: 12, about: 'UNC reached the NCAA quarterfinals, demonstrating their continued strength in womens soccer.' },
+						{ name: '2022', value: 20, about: 'UNC finished as runners-up.' }
 					]
 				}
 			]
 		},
 		{
 			name: 'Championships',
+			about: 'The Tar Heels have won 22 NCAA Womens Soccer Championships, the most of any program in the nation. They have also won 21 ACC Womens Soccer Championships, the most of any program in the conference.',
 			children: [
                 {
                     name: 'NCAA Womens Soccer Championships',
@@ -202,13 +205,12 @@ export default {
 							]
 						},
 						{
-							name: '2019 - 2023',
+							name: '2019 - 2022',
 							children: [
 								{ name: '2019', value: 0, about: 'Runner up' },
 								{ name: '2020', value: 0, about: 'Semifinals' },
 								{ name: '2021', value: 0 },
-								{ name: '2022', value: 0, about: 'Runner up' },
-								{ name: '2023', value: 0, about: 'Runner up' }
+								{ name: '2022', value: 0, about: 'Runner up' }
 							]
 						}
                     ]
@@ -298,13 +300,12 @@ export default {
 							]
 						},
 						{
-							name: '2019 - 2023',
+							name: '2019 - 2022',
 							children: [
 								{ name: '2019', value: 1 },
 								{ name: '2020', value: 0 },
 								{ name: '2021', value: 0 },
-								{ name: '2022', value: 0 },
-								{ name: '2023', value: 0 }
+								{ name: '2022', value: 0 }
 							]
 						}
 					]
