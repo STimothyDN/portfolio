c15f8ab31ebf0e58e3e8cd2004aae683e9677758
Timothy Navarro <stimothydn@gmail.com>
2023-05-01 07:45:06 -0400

    final project

---
+++ src/components/data.js
@@ -0,0 +1,315 @@
+
+
+
+
+export default {
+	name: 'flare',
+	children: [
+		{
+			name: 'Wins',
+			children: [
+				{
+					name: '1979 - 1983',
+					about: 'The beginnings of a dynasty.',
+					children: [
+						{ name: '1979', value: 10, about: 'The UNC Womens Soccer Team was established and quickly gained recognition in collegiate soccer.'},
+						{ name: '1980', value: 21, about: 'AIAW Semifinals'},
+						{ name: '1981', value: 23, about: 'UNC captured their first AIAW National Championship, foreshadowing their future dominance.' },
+						{ name: '1982', value: 19, about: 'The Tar Heels claimed their first NCAA National Championship, beginning a new era in womens soccer.' },
+						{ name: '1983', value: 19, about: 'The team won back-to-back NCAA titles, solidifying their position as a collegiate powerhouse.' }
+					]
+				},
+				{
+					name: '1984 - 1988',
+					about: 'Building up steam.',
+					children: [
+						{ name: '1984', value: 24, about: 'The team won back-to-back NCAA titles, solidifying their position as a collegiate powerhouse.' },
+						{ name: '1985', value: 18, about: 'Runner up' },
+						{ name: '1986', value: 24, about: 'The Tar Heels begin an unparalleled nine-year championship streak, showcasing their dominance.' },
+						{ name: '1987', value: 23 },
+						{ name: '1988', value: 18 }
+					]
+				},
+				{
+					name: '1989 - 1993',
+					about: 'Dominance solidified',
+					children: [
+						{ name: '1989', value: 24 },
+						{ name: '1990', value: 20 },
+						{ name: '1991', value: 24 },
+						{ name: '1992', value: 25 },
+						{ name: '1993', value: 1 }
+					]
+				},
+				{
+					name: '1994 - 1998',
+					about: 'Reputation building',
+					children: [
+						{ name: '1994', value: 1 },
+						{ name: '1995', value: 0, about: 'Semifinals' },
+						{ name: '1996', value: 1, about: 'The team reclaimed their championship status, winning back-to-back NCAA titles.' },
+						{ name: '1997', value: 1, about: 'The team reclaimed their championship status, winning back-to-back NCAA titles.' },
+						{ name: '1998', value: 0, about: 'Runner up' }
+					]
+				},
+				{
+					name: '1999 - 2003',
+					about: 'Competition stiffens',
+					children: [
+						{ name: '1999', value: 1, about: 'The Tar Heels secured consecutive national championships, furthering their exceptional legacy.' },
+						{ name: '2000', value: 1, about: 'The Tar Heels secured consecutive national championships, furthering their exceptional legacy.' },
+						{ name: '2001', value: 0, about: 'Runner up' },
+						{ name: '2002', value: 0, about: 'Semifinals' },
+						{ name: '2003', value: 1, about: 'UNC claimed their 18th NCAA title, reasserting their dominance in collegiate soccer.' }
+					]
+				},
+				{
+					name: '2004 - 2008',
+					about: 'Tar Heels remain competitive',
+					children: [
+						{ name: '2004', value: 0 },
+						{ name: '2005', value: 0 },
+						{ name: '2006', value: 1, about: 'The team won their 19th NCAA title, reestablishing their championship pedigree.' },
+						{ name: '2007', value: 0 },
+						{ name: '2008', value: 1, about: 'The Tar Heels captured their 20th NCAA title, asserting their prowess on the national stage.' }
+					]
+				},
+				{
+					name: '2009 - 2013',
+					about: 'The dynasty begins to crumble',
+					children: [
+						{ name: '2009', value: 1, about: 'UNC won their 21st national championship, further solidifying their place in womens soccer history.' },
+						{ name: '2010', value: 0 },
+						{ name: '2011', value: 0 },
+						{ name: '2012', value: 1, about: 'The Tar Heels claimed their 22nd NCAA title, reemphasizing their championship caliber. This is their last NCAA championship title.' },
+						{ name: '2013', value: 0 }
+					]
+				},
+				{
+					name: '2014 - 2018',
+					children: [
+						{ name: '2014', value: 0 },
+						{ name: '2015', value: 0 },
+						{ name: '2016', value: 0, about: 'Semifinals' },
+						{ name: '2017', value: 0 },
+						{ name: '2018', value: 0, about: 'Runner up' }
+					]
+				},
+				{
+					name: '2019 - 2023',
+					children: [
+						{ name: '2019', value: 0, about: 'Runner up' },
+						{ name: '2020', value: 0, about: 'Semifinals' },
+						{ name: '2021', value: 0 },
+						{ name: '2022', value: 0, about: 'Runner up' },
+						{ name: '2023', value: 0, about: 'Runner up' }
+					]
+				}
+			]
+		},
+		{
+			name: 'Championships',
+			children: [
+                {
+                    name: 'NCAA Womens Soccer Championships',
+					about: 'The NCAA began conducting a single division Women\'s Soccer Championship tournament in 1982. The tournament became the Division I Championship in 1991, when Division III was created for non-scholarship programs. The most successful program is North Carolina, with 21 titles.',
+                    children: [
+                        {
+                            name: '1979 - 1983',
+							about: 'The beginnings of a dynasty.',
+                            children: [
+                                { name: '1979', value: 0, about: 'The UNC Womens Soccer Team was established and quickly gained recognition in collegiate soccer.'},
+						        { name: '1980', value: 0, about: 'AIAW Semifinals'},
+						        { name: '1981', value: 1, about: 'UNC captured their first AIAW National Championship, foreshadowing their future dominance.' },
+						        { name: '1982', value: 1, about: 'The Tar Heels claimed their first NCAA National Championship, beginning a new era in womens soccer.' },
+                                { name: '1983', value: 1, about: 'The team won back-to-back NCAA titles, solidifying their position as a collegiate powerhouse.' }
+                            ]
+                        },
+						{
+							name: '1984 - 1988',
+							about: 'Building up steam.',
+							children: [
+								{ name: '1984', value: 1, about: 'The team won back-to-back NCAA titles, solidifying their position as a collegiate powerhouse.' },
+								{ name: '1985', value: 0, about: 'Runner up' },
+								{ name: '1986', value: 1, about: 'The Tar Heels begin an unparalleled nine-year championship streak, showcasing their dominance.' },
+								{ name: '1987', value: 1 },
+								{ name: '1988', value: 1 }
+							]
+						},
+						{
+							name: '1989 - 1993',
+							about: 'Dominance solidified',
+							children: [
+								{ name: '1989', value: 1 },
+								{ name: '1990', value: 1 },
+								{ name: '1991', value: 1 },
+								{ name: '1992', value: 1 },
+								{ name: '1993', value: 1 }
+							]
+						},
+						{
+							name: '1994 - 1998',
+							about: 'Reputation building',
+							children: [
+								{ name: '1994', value: 1 },
+								{ name: '1995', value: 0, about: 'Semifinals' },
+								{ name: '1996', value: 1, about: 'The team reclaimed their championship status, winning back-to-back NCAA titles.' },
+								{ name: '1997', value: 1, about: 'The team reclaimed their championship status, winning back-to-back NCAA titles.' },
+								{ name: '1998', value: 0, about: 'Runner up' }
+							]
+						},
+						{
+							name: '1999 - 2003',
+							about: 'Competition stiffens',
+							children: [
+								{ name: '1999', value: 1, about: 'The Tar Heels secured consecutive national championships, furthering their exceptional legacy.' },
+								{ name: '2000', value: 1, about: 'The Tar Heels secured consecutive national championships, furthering their exceptional legacy.' },
+								{ name: '2001', value: 0, about: 'Runner up' },
+								{ name: '2002', value: 0, about: 'Semifinals' },
+								{ name: '2003', value: 1, about: 'UNC claimed their 18th NCAA title, reasserting their dominance in collegiate soccer.' }
+							]
+						},
+						{
+							name: '2004 - 2008',
+							about: 'Tar Heels remain competitive',
+							children: [
+								{ name: '2004', value: 0 },
+								{ name: '2005', value: 0 },
+								{ name: '2006', value: 1, about: 'The team won their 19th NCAA title, reestablishing their championship pedigree.' },
+								{ name: '2007', value: 0 },
+								{ name: '2008', value: 1, about: 'The Tar Heels captured their 20th NCAA title, asserting their prowess on the national stage.' }
+							]
+						},
+						{
+							name: '2009 - 2013',
+							about: 'The dynasty begins to crumble',
+							children: [
+								{ name: '2009', value: 1, about: 'UNC won their 21st national championship, further solidifying their place in womens soccer history.' },
+								{ name: '2010', value: 0 },
+								{ name: '2011', value: 0 },
+								{ name: '2012', value: 1, about: 'The Tar Heels claimed their 22nd NCAA title, reemphasizing their championship caliber. This is their last NCAA championship title.' },
+								{ name: '2013', value: 0 }
+							]
+						},
+						{
+							name: '2014 - 2018',
+							children: [
+								{ name: '2014', value: 0 },
+								{ name: '2015', value: 0 },
+								{ name: '2016', value: 0, about: 'Semifinals' },
+								{ name: '2017', value: 0 },
+								{ name: '2018', value: 0, about: 'Runner up' }
+							]
+						},
+						{
+							name: '2019 - 2023',
+							children: [
+								{ name: '2019', value: 0, about: 'Runner up' },
+								{ name: '2020', value: 0, about: 'Semifinals' },
+								{ name: '2021', value: 0 },
+								{ name: '2022', value: 0, about: 'Runner up' },
+								{ name: '2023', value: 0, about: 'Runner up' }
+							]
+						}
+                    ]
+                },
+				{
+					name: 'ACC Womens Soccer Championships',
+					about: 'The ACC has held a single division Women\'s Soccer Championship tournament since 1988.',
+					children: [
+						{
+                            name: '1979 - 1983',
+                            children: [
+                                { name: '1979', value: 0 },
+						        { name: '1980', value: 0 },
+						        { name: '1981', value: 0 },
+						        { name: '1982', value: 0 },
+                                { name: '1983', value: 0 }
+                            ]
+                        },
+						{
+							name: '1984 - 1988',
+							children: [
+								{ name: '1984', value: 0 },
+								{ name: '1985', value: 0 },
+								{ name: '1986', value: 0 },
+								{ name: '1987', value: 0 },
+								{ name: '1988', value: 0 }
+							]
+						},
+						{
+							name: '1989 - 1993',
+							children: [
+								{ name: '1989', value: 1 },
+								{ name: '1990', value: 1 },
+								{ name: '1991', value: 1 },
+								{ name: '1992', value: 1 },
+								{ name: '1993', value: 1 }
+							]
+						},
+						{
+							name: '1994 - 1998',
+							children: [
+								{ name: '1994', value: 1 },
+								{ name: '1995', value: 1 },
+								{ name: '1996', value: 1 },
+								{ name: '1997', value: 1 },
+								{ name: '1998', value: 1 }
+							]
+						},
+						{
+							name: '1999 - 2003',
+							children: [
+								{ name: '1999', value: 1 },
+								{ name: '2000', value: 1 },
+								{ name: '2001', value: 1 },
+								{ name: '2002', value: 1 },
+								{ name: '2003', value: 1 }
+							]
+						},
+						{
+							name: '2004 - 2008',
+							children: [
+								{ name: '2004', value: 0 },
+								{ name: '2005', value: 1 },
+								{ name: '2006', value: 1 },
+								{ name: '2007', value: 1 },
+								{ name: '2008', value: 1 }
+							]
+						},
+						{
+							name: '2009 - 2013',
+							children: [
+								{ name: '2009', value: 1 },
+								{ name: '2010', value: 0 },
+								{ name: '2011', value: 0 },
+								{ name: '2012', value: 0 },
+								{ name: '2013', value: 0 }
+							]
+						},
+						{
+							name: '2014 - 2018',
+							children: [
+								{ name: '2014', value: 0 },
+								{ name: '2015', value: 0 },
+								{ name: '2016', value: 0 },
+								{ name: '2017', value: 1 },
+								{ name: '2018', value: 0 }
+							]
+						},
+						{
+							name: '2019 - 2023',
+							children: [
+								{ name: '2019', value: 1 },
+								{ name: '2020', value: 0 },
+								{ name: '2021', value: 0 },
+								{ name: '2022', value: 0 },
+								{ name: '2023', value: 0 }
+							]
+						}
+					]
+				},
+			]
+		}
+	]
+};
